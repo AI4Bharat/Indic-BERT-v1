9b5ec8a642a6e6b4615ff8fd20cf76ba6ee2368c
divkakwani <divkakwani@gmail.com>
2020-05-21 01:56:11 +0530

    update pretrain sript

--- scripts/create_masked_data.sh
+++ scripts/create_masked_data.sh
@@ -1,11 +1,12 @@
 
-python3 albert/create_pretraining_data.py \
-  --input_file=sents/smoothed_all.txt \
-  --output_file=train.tf_record  \
-  --spm_model_file=indicnlp-tokenizer/multilingual/mult2.spm.unigram.100k.model \
-  --vocab_file=indicnlp-tokenizer/multilingual/mult2.spm.unigram.100k.vocab \
+ls shards | xargs -I {} python3 albert/create_pretraining_data.py \
+  --input_file=shards/{} \
+  --output_file=pretrain_data/{}.tf_record  \
+  --spm_model_file=indicnlp-tokenizer/multilingual/translit1/spm.unigram.100k.model \
+  --vocab_file=indicnlp-tokenizer/multilingual/translit1/spm.unigram.100k.vocab \
   --max_seq_length=128 \
   --max_predictions_per_seq=20 \
   --masked_lm_prob=0.15 \
   --random_seed=12345 \
-  --dupe_factor=2
+  --dupe_factor=3
+
