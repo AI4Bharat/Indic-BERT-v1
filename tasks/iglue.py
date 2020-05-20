58e12582353c5648c2051ad34887721c042e6a1b
Divyanshu Kakwani <divkakwani@gmail.com>
2020-05-20 23:42:55 +0530

    update iglue

--- tasks/iglue.py
+++ tasks/iglue.py
@@ -2,49 +2,48 @@
 
 import logging
 import os
+import csv
 from enum import Enum
 from typing import List, Optional, Union
 
-from transformers.data.processors.utils import SingleSentenceClassificationProcessor, DataProcessor
+from transformers.data.processors.utils import DataProcessor
 from transformers.data.processors.utils import InputExample, InputFeatures
 
 
-def read_csv(cls, input_file, quotechar=None):
+def read_csv(input_file):
     """Reads a comma separated value file."""
     with open(input_file, "r", encoding="utf-8-sig") as f:
-        return list(csv.reader(f, delimiter=",", quotechar=quotechar))
-
-# monkey-patch read tsv method
-DataProcessor._read_tsv = read_csv
+        return list(csv.reader(f, delimiter=","))
 
 
-class AGCProcessor(SingleSentenceClassificationProcessor):
+class AGCProcessor(DataProcessor):
     """Processor for the Article Genre Classification data set"""
 
     def get_train_examples(self, data_dir):
         """See base class."""
-        return self.create_from_csv(os.path.join(data_dir, "kn-train.csv"), "train")
+        return self._create_examples(read_csv(os.path.join(data_dir, "kn-train.csv")), "train")
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        return self.create_from_csv(os.path.join(data_dir, "kn-test.csv"), "dev")
+        return self._create_examples(read_csv(os.path.join(data_dir, "kn-test.csv")), "dev")
 
-    def get_labels(self):
+    def get_labels(self, data_dir):
         """See base class."""
-        return ["entailment", "not_entailment"]
+        lines = read_csv(os.path.join(data_dir, "kn-train.csv"))
+        labels = map(lambda l: l[0], lines)
+        labels = list(set(labels))
+        return labels
 
-# remove
-iglue_tasks_num_labels = {
-    "cola": 2,
-    "mnli": 3,
-    "mrpc": 2,
-    "sst-2": 2,
-    "sts-b": 1,
-    "qqp": 2,
-    "qnli": 2,
-    "rte": 2,
-    "wnli": 2,
-}
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            guid = "%s-%s" % (set_type, i)
+            text_a = line[1]
+            label = line[0]
+            examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
+
 
 iglue_processors = {
     "agc": AGCProcessor,
