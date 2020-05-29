b8688c946a0bf901f9fa3d5ce2d3bc90b825677f
divkakwani <divkakwani@gmail.com>
2020-05-30 02:21:45 +0530

    apply hack in mep

--- tasks/missing-entity-prediction/run_model.py
+++ tasks/missing-entity-prediction/run_model.py
@@ -41,6 +41,18 @@ class MEPTransformer(LightningBase):
         self.test_results_fpath = 'test_results'
         if os.path.exists(self.test_results_fpath):
             os.remove(self.test_results_fpath)
+    
+        (candidates, answers) = self.processor.get_test_options(self.hparams.data_dir)
+
+        self.answers = answers
+
+        self.candidates_ids = {}
+
+        for i, candidate in candidates.items():
+            candidate = [max(self.tokenizer.tokenize(cand), key=lambda t: len(t.strip(string.punctuation)))
+                         for cand in candidate]
+            candidate_ids = self.tokenizer.convert_tokens_to_ids(candidate)
+            self.candidates_ids[i] = candidate_ids
 
     def forward(self, **inputs):
         return self.model(**inputs)
@@ -75,47 +87,49 @@ class MEPTransformer(LightningBase):
         predictions = predictions[np.arange(predictions.shape[0]), mask_ids, :]
         outputs = np.hstack([labels[:, None], predictions])
 
-        return {"outputs": outputs}
+        pred_answers = {}
+        for pred in predictions:
+            cands = self.candidates_ids[pred[0]]
+            prob = pred[1:]
+            prob = prob[cands]
+            pred_answers[pred[0]] = int(np.argmax(prob))
+
+        return {"outputs": pred_answers}
 
     def test_epoch_end(self, outputs):
-        all_outputs = np.vstack([x["outputs"] for x in outputs])
+        # all_outputs = np.vstack([x["outputs"] for x in outputs])
+
+        # with FileLock(self.test_results_fpath + '.lock'):
+        #     if os.path.exists(self.test_results_fpath):
+        #         with open(self.test_results_fpath, 'rb') as fp:
+        #             data = pickle.load(fp)
+        #         data = np.vstack([data, all_outputs])
+        #     else:
+        #         data = all_outputs
+        #     with open(self.test_results_fpath, 'wb') as fp:
+        #         pickle.dump(data, fp)
+
+        # return {"outputs": all_outputs}
+
+        merged = {**output for output in outputs}
 
         with FileLock(self.test_results_fpath + '.lock'):
             if os.path.exists(self.test_results_fpath):
                 with open(self.test_results_fpath, 'rb') as fp:
                     data = pickle.load(fp)
-                data = np.vstack([data, all_outputs])
+                data = {**merged, **data} 
             else:
-                data = all_outputs
+                data = merged 
             with open(self.test_results_fpath, 'wb') as fp:
                 pickle.dump(data, fp)
 
-        return {"outputs": all_outputs}
+        return {"outputs": merged}
 
     @staticmethod
     def add_model_specific_args(parser, root_dir):
         LightningBase.add_model_specific_args(parser, root_dir)
         return parser
 
-"""
-        for example in self.processors.get_test_examples(self.hparams.data_dir):
-	    candidates = example['candidates']
-
-
-            indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-            segments_ids = [0] * len(tokenized_text)
-            tokens_tensor = torch.tensor([indexed_tokens])
-            segments_tensors = torch.tensor([segments_ids])
-
-            language_model = BertForMaskedLM.from_pretrained('bert-base-uncased')
-            language_model.eval()
-
-            predictions = language_model(tokens_tensor, segments_tensors)
-            predictions_candidates = predictions[0, masked_index, candidates_ids]
-            answer_idx = torch.argmax(predictions_candidates).item()
-
-            print(f'The most likely word is "{candidates[answer_idx]}".')
-"""
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -136,33 +150,20 @@ if __name__ == "__main__":
 
     trainer.test(model)
     preds = pickle.load(open(model.test_results_fpath, 'rb'))
+    answers = model.answers
 
     os.remove(model.test_results_fpath)
 
-
-    (candidates, answers) = model.processor.get_test_options(args.data_dir)
-
-    candidates_ids = {}
-    for i, candidate in candidates.items():
-        candidate = [max(model.tokenizer.tokenize(cand), key=lambda t: len(t.strip(string.punctuation)))
-                     for cand in candidate]
-        candidate_ids = model.tokenizer.convert_tokens_to_ids(candidate)
-        candidates_ids[i] = candidate_ids
-
-    pred_answers = {}
-    for pred in preds:
-        cands = candidates_ids[pred[0]]
-        prob = pred[1:]
-        prob = prob[cands]
-        pred_answers[pred[0]] = int(np.argmax(prob))
-
     correct, wrong = 0, 0
-    for i in range(100000):
-        if i in pred_answers and i in answers:
+    for i in answers:
+        if i in pred_answers:
             if pred_answers[i] == answers[i]:
                 correct += 1
             else:
                 wrong += 1
+        else:
+            raise 'Prediction not found'
+
     print('Accuracy: ', correct/(correct + wrong))
 
 
