938d8485a76861f9d3ef0a7be9fd661bce34794b
Divyanshu Kakwani <divkakwani@gmail.com>
2020-07-21 18:22:08 +0530

    add tensorflow to pytorch conversion script

---
+++ scripts/convert_to_pt.sh
@@ -0,0 +1,7 @@
+
+export ALBERT_BASE_DIR=$1
+
+transformers-cli convert --model_type albert \
+  --tf_checkpoint $ALBERT_BASE_DIR/model.ckpt \
+  --config $ALBERT_BASE_DIR/config.json \
+  --pytorch_dump_output $ALBERT_BASE_DIR/pytorch_model.bin
