9dbe20cdfbe99f60928b47753be79124b38932cd
Divyanshu Kakwani <divkakwani@gmail.com>
2020-09-01 22:52:30 +0530

    bug fix

--- fine_tune/data/processors.py
+++ fine_tune/data/processors.py
@@ -424,7 +424,7 @@ class WNLI(DataProcessor):
             guid = "%s-%s" % (set_type, line[0])
             text_a = line[1]
             text_b = line[2]
-            label = None if set_type == 'test' else line[-1]
+            label = line[-1]
             examples.append(TextExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
 
