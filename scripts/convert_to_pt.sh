6ed4ba85f1db2663e0d878f4e40ed4077dfd3002
divkakwani <divkakwani@gmail.com>
2020-10-01 22:32:03 +0000

    update convert to pt script

--- scripts/convert_to_pt.sh
+++ scripts/convert_to_pt.sh
@@ -2,6 +2,6 @@
 export ALBERT_BASE_DIR=$1
 
 transformers-cli convert --model_type albert \
-  --tf_checkpoint $ALBERT_BASE_DIR/model.ckpt \
+  --tf_checkpoint $ALBERT_BASE_DIR/tf_model \
   --config $ALBERT_BASE_DIR/config.json \
   --pytorch_dump_output $ALBERT_BASE_DIR/pytorch_model.bin
